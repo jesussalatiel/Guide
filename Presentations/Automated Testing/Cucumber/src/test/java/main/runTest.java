61483bcc0b12dd76bec5ac67eba5f23090210ccf
Jesus Salatiel <jesussalatiel@hotmail.com>
2021-04-29 10:43:13 -0500

    Resources

--- Presentations/Automated Testing/Cucumber/src/test/java/main/runTest.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
-
-@CucumberOptions(features = "src/test/java/features/search.feature", glue = "steps")
-public class runTest extends AbstractTestNGCucumberTests {
-
-}
